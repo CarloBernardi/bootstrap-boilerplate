796446602c7419af007faa39e911624a8aa2c945
Carlo Bernardi <carletto90@gmail.com>
2017-10-19 16:52:35 +0200

    favicons regenerated

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#555555">
+
+    <link rel="shortcut icon" href="./dist/img/favicon/favicon.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="./dist/img/favicon/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./dist/img/favicon/favicon-32x32.png">
+
     <link rel="stylesheet" href="./dist/css/app.css">
-    <link rel="icon" type="image/png" href="./dist/img/favicon/favicon-32x32.png" sizes="32x32"/>
-    <link rel="icon" type="image/png" href="./dist/img/favicon/favicon-16x16.png" sizes="16x16"/>
+
     <title>Bootstrap 4 Boilerplate</title>
 </head>
 <body class="h-100">
